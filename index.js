8f5b4077097572018cef27ae5d6ac70f729b3c5f
MIBICT <mladen.ivosevic@gmail.com>
2022-07-11 04:50:49 +0200

    dotjerivanje koda

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-// random value generated
+// funkcija generira slučajni broj
 var y = Math.floor(Math.random() * 10 + 1);
-
+// za testiranje u konzoli
+console.log(y)
 // brojanje pokušaja
 var guess = 1;
+// dohvaćanje unosa
+document.getElementById("uneseniBroj").onclick = function () {
 
-document.getElementById("uneseniBroj").onclick = function() {
-
-    // unos pretpostavljenog (pogađanog) broja     
+// postavljanje nove varijable     
     var x = document.getElementById("poljeUnos").value;
-
-    if (x == y) {
+// provjera da li je u unos u rasponu 1 - 10
+    if (x > 10 || x < 1) {
+        alert("Unesite broj između 1 i 10");
+    } 
+    // provjera da li je odgovor točan
+    else if (x == y) {
         alert("Bravo! točno ste pogodili iz " +
-            guess + " pokušaja!");
-    } else if (x > y)
-    /* ako je uneseni broj veći od generiranog */
-    {
+            guess + " pokušaja! ");
+// učitava iznova stranicu kako bi se generirao novi broj
+            document.location.reload();   
+    }
+// ako odgovor nije točan - provjera da li odgovor veći od generiranog broja
+    else if (x > y) {
+// povećava broj neuspješnih pokušaja za 1
         guess++;
         alert("Ne baš... zamišljeni broj je manji od vašeg pokušaja.");
-        /* ako je uneseni broj manji od generiranog */
-    } else {
+    } 
+// ako odgovor nije veći (a nije ni točan) onda je manji 
+    else {
+// povećava broj neuspješnih pokušaja za 1
         guess++;
         alert("Ne baš... zamišljeni broj je veći od vašeg pokušaja.")
     }
