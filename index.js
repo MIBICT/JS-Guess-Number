305319d5350000038dd93da8f4624d2d89c902c8
MIBICT <mladen.ivosevic@gmail.com>
2022-07-11 16:19:14 +0200

    final v1

--- index.js
+++ index.js
@@ -19,24 +19,22 @@ document.getElementById("uneseniBroj").onclick = function () {
 // HR - provjera da li je u unos u rasponu 1 - 10
 // EN - checking if input is in range 1 - 10
     if (x > 10 || x < 1) {
-        alert("Unesite broj između 1 i 10");
+// HR - ispisuje upozorenje o pogrešci
+// EN - prints warning about error
+document.getElementById("poruka").innerHTML = "Neispravan unos!<br />Unesite broj između 1 i 10.";
     }
 // HR - provjera da li je odgovor točan
 // EN - checking if answer is correct
     else if (x == y) {
-        alert("Bravo! točno ste pogodili iz " +
-            guess + " pokušaja! ");
-// HR - učitava iznova stranicu kako bi se generirao novi broj
-// EN - reloading page to generate new number
-            document.location.reload();
-    }
+        document.getElementById("poruka").innerHTML = "Bravo! točno ste pogodili iz " + guess + " pokušaja!<br />Pritisnite 'Novi broj' da bi ste igrali ponovo.";
+   }
 // HR - ako odgovor nije točan - provjera da li je odgovor veći od generiranog broja
 // EN - if answer is not correct - checking if answer is bigger than generated number
     else if (x > y) {
 // HR - povećava broj neuspješnih pokušaja za 1
 // EN - increasing unsuccessful attempts by 1
         guess++;
-        alert("Ne baš... zamišljeni broj je manji od vašeg pokušaja.");
+        document.getElementById("poruka").innerHTML = "Ne baš... zamišljeni broj je MANJI od vašeg pokušaja.";
     }
 // HR - ako odgovor nije veći od generiranog broja (a nije ni točan) onda je manji
 // EN - if answer is not bigger than generated number (and not correct) then it is smaller
@@ -44,6 +42,6 @@ document.getElementById("uneseniBroj").onclick = function () {
 // HR - povećava broj neuspješnih pokušaja za 1
 // EN - increasing unsuccessful attempts by 1
         guess++;
-        alert("Ne baš... zamišljeni broj je veći od vašeg pokušaja.");
+        document.getElementById("poruka").innerHTML = "Ne baš... zamišljeni broj je VEĆI od vašeg pokušaja.";
     }
 }
